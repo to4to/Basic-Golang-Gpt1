3e849e6273aff9233cec4bab1ca2fd203dff1262
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-08-02 16:08:31 +0530

    starting with api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/to4to/Basic-Golang-gpt1
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
+import (
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load()
+
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatal("Missing Api Key")
+	}
 }
